ea9ff475f2e95f0a8606a012a07f1bd27b05af2a
Lorena Criado <criado.manzaneque@gmail.com>
2024-08-08 20:12:31 +0200

    First fourth iterations are working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
+const Recipe = require('./models/Recipe.model.js');
 
 const app = express();
 
@@ -11,7 +12,14 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const mongoose = require("mongoose");
 
+// Conexión a DB y crear express-mongoose-recipes-dev
+const MONGODB_URI = 'mongodb+srv://criadomanzaneque:MSNvQed7qIZgA387@cluster0.fl8rdre.mongodb.net/express-mongoose-recipes-dev'; 
+
+mongoose.connect(MONGODB_URI)
+.then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+.catch((err) => console.error("Error connecting to mongo", err));
 
 
 // ROUTES
@@ -23,11 +31,39 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
+app.post('/recipes', (req, res) => {
+    // 1. Leer toda la información 
+    const { title, instructions, level, ingredients, image, duration, isArchived, created } = req.body;
 
+    Recipe.create({
+        title,
+        instructions,
+        level,
+        ingredients,
+        image,
+        duration,
+        isArchived,
+        created
+    }).then((createdRecipe) => {
+        res.status(201).json(createdRecipe);
+    }).catch((err) => {
+        res.status(500).json({
+            message: "Error creating new recipe: " + err.message
+        })
+    })
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
+app.get('/recipes', async (req, res) => {
+    try {
+        const recipes = await Recipe.find();
+        res.json(recipes);
 
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,41 @@
-// Your code here ...
+const { Schema, model } = require('mongoose');
+
+// Creamos esquema
+const recipeSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    instructions: {
+        type: String,
+        required: true
+    },
+    level: {
+        type: String,
+        enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef']
+    },
+    ingredients: [String],
+    image: {
+        type: String,
+        default: "https://images.media-allrecipes.com/images/75131.jpg"
+    },
+    duration: {
+        type: Number,
+        min: 0
+    },
+    isArchived: {
+        type: Boolean,
+        default: false
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+// Asociar esquema creado al modelo
+const Recipe = new model('Recipe', recipeSchema)
+
+// Esportamos módulo Receta
+module.exports = Recipe
